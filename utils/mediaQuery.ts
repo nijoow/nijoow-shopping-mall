a90b11c0d2d837c16770d0d14ad8c942b75f42eb
Woojin Lee <nijoow1127@gmail.com>
2023-01-26 01:50:08 +0900

    🐛 hydration 에러 해결

--- utils/mediaQuery.ts
+++ utils/mediaQuery.ts
@@ -1,12 +1,24 @@
+import { useEffect, useState } from 'react';
 import { useMediaQuery } from 'react-responsive';
 
 const Mobile = ({ children }: { children: JSX.Element }): JSX.Element | null => {
-  const isMobile = useMediaQuery({ maxWidth: 767 });
+  const [isMobile, setIsMobile] = useState(false);
+  const mobile = useMediaQuery({ maxWidth: 767 });
+
+  useEffect(() => {
+    setIsMobile(mobile);
+  }, [mobile]);
 
   return isMobile ? children : null;
 };
+
 const Desktop = ({ children }: { children: JSX.Element }): JSX.Element | null => {
-  const isDesktop = useMediaQuery({ minWidth: 768 });
+  const [isDesktop, setIsDesktop] = useState(false);
+  const desktop = useMediaQuery({ minWidth: 768 });
+
+  useEffect(() => {
+    setIsDesktop(desktop);
+  }, [desktop]);
 
   return isDesktop ? children : null;
 };
