425d72a1e684668dd35403d86b41a32666c8abc3
Woojin Lee <nijoow1127@gmail.com>
2023-02-01 01:54:39 +0900

    🎨 useAuth 코드 구조 변경

--- pages/hooks/useAuth.tsx
+++ pages/hooks/useAuth.tsx
@@ -1,49 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import { Session, User } from '@supabase/supabase-js';
-import { supabase } from '../../lib/supabase';
+import { useState } from 'react';
 import { useRouter } from 'next/router';
+import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react';
 
 const useAuth = () => {
+  const session = useSession();
+  const supabase = useSupabaseClient();
   const [loading, setLoading] = useState(false);
-  const [user, setUser] = useState<User | null>(null);
-  const [loggedIn, setLoggedIn] = useState(false);
   const router = useRouter();
-  const [userLoaded, setUserLoaded] = useState(false);
-  const [session, setSession] = useState<Session | null>(null);
-
-  useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session);
-      setUserLoaded(session ? true : false);
-      if (session?.user) {
-        console.log('seesion!');
-      }
-    });
-
-    const { data } = supabase.auth.onAuthStateChange(async (event, session) => {
-      setSession(session);
-      const currentUser = session?.user;
-      setUser(currentUser ?? null);
-      setUserLoaded(!!currentUser);
-      if (currentUser) {
-        // logIn(currentUser.id, currentUser.email);
-        console.log(currentUser);
-      }
-    });
-
-    return () => {
-      data.subscription.unsubscribe();
-    };
-  }, []);
 
   const logIn = async (payload: { email: string; password: string }) => {
     try {
       setLoading(true);
-      const { error, data } = await supabase.auth.signInWithPassword(payload);
+      const { error } = await supabase.auth.signInWithPassword(payload);
       if (error) {
         console.log(error);
       } else {
-        console.log(data);
         router.push('/');
       }
     } catch (error) {
@@ -52,7 +23,7 @@ const useAuth = () => {
       setLoading(false);
     }
   };
-  const logOut = async () => await supabase.auth.signOut();
+  const logOut = async () => await supabase.auth.signOut().then(() => router.push('/login'));
 
   const signUp = async (payload: { email: string; password: string }) => {
     try {
@@ -70,7 +41,7 @@ const useAuth = () => {
     }
   };
 
-  return { logIn, logOut, signUp };
+  return { logIn, logOut, signUp, loading, session };
 };
 
 export default useAuth;
