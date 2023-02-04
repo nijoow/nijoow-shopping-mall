d1a5a5f27e72927587ae9a7a3121b3b8b3484351
Woojin Lee <nijoow1127@gmail.com>
2023-02-04 16:19:47 +0900

    🔧 tailwind 속성 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './app/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: { texture: 'url(/texture.png)' },
+      fontFamily: {
+        Insomnia: 'Insomnia',
+      },
+      colors: {
+        brown: '#6F5643',
+        orange: '#CC6B49',
+        ocher: '#D2A24C',
+        beige: '#ECE6C2',
+        mint: '#73BDA8',
+      },
+    },
   },
   plugins: [],
-}
+};
