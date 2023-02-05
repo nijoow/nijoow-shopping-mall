08b094135a660b81ec9fe1841c470ba94675e197
Woojin Lee <nijoow1127@gmail.com>
2023-02-06 00:19:28 +0900

    ✨ footer 컴포넌트 생성

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="flex items-center flex-none justify-center w-full h-32 bg-beige/40 text-brown">
+      <div className="flex items-center w-full h-full gap-20 mx-auto max-w-7xl px-4">footer</div>{' '}
+    </div>
+  );
+};
+
+export default Footer;
