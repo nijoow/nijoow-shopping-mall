f97ab4e9627c99afe3120f5152a8a46d071ee846
Woojin Lee <nijoow1127@gmail.com>
2023-02-06 00:20:01 +0900

    💄 layout, navbar css 수정

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,40 +9,38 @@ const Navbar = () => {
   const { session, logOut } = useAuth();
 
   return (
-    <nav className="relative w-full h-12 overflow-hidden backdrop-opacity-100 text-brown ">
-      <div className="flex items-center w-full h-full gap-20 mx-auto max-w-7xl ">
-        <Link href="/" className="ml-4 text-lg font-medium cursor-pointer font-Insomnia">
+    <nav className="relative w-full flex-none h-12 overflow-hidden bg-beige/40 text-brown">
+      <div className="flex items-center w-full h-full gap-20 mx-auto max-w-7xl px-4">
+        <Link href="/" className="text-lg font-medium cursor-pointer font-Insomnia">
           nijoow vintage
         </Link>
         <Desktop>
           <>
             <ul className="flex gap-12 text-sm font-medium ">
-              <li className="cursor-pointer font-Insomnia">menu</li>
-              <li className="cursor-pointer font-Insomnia">menu</li>
-              <li className="cursor-pointer font-Insomnia">menu</li>
+              <li className="cursor-pointer">SHOP</li>
             </ul>{' '}
             <div className="flex-auto"></div>
             <ul className="flex gap-8 text-sm ">
               {session ? (
                 <>
                   {' '}
-                  <Link href="/profile">
+                  <Link href="/user/profile">
                     <li className="flex items-center justify-center w-5 h-5 border rounded-full cursor-pointer border-zinc-200">
                       <AiOutlineUser size={20} />
                     </li>{' '}
                   </Link>{' '}
                   <div onClick={() => logOut()}>
-                    <li className="cursor-pointer font-Insomnia">LogOut</li>{' '}
+                    <li className="cursor-pointer">로그아웃</li>{' '}
                   </div>
                 </>
               ) : (
                 <>
                   {' '}
                   <Link href="/login">
-                    <li className="cursor-pointer font-Insomnia">Login</li>{' '}
+                    <li className="cursor-pointer">로그인</li>{' '}
                   </Link>
                   <Link href="/signup">
-                    <li className="cursor-pointer font-Insomnia">SignUp</li>
+                    <li className="cursor-pointer">회원가입</li>
                   </Link>
                 </>
               )}
