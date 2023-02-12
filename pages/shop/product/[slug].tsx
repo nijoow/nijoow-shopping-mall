3f5d4d4b65e685cdc2c59c2ea5a7773489b3d7b0
Woojin Lee <nijoow1127@gmail.com>
2023-02-13 01:47:30 +0900

    🚧 상품페이지 섹션 및 스크롤 버튼 추가

--- pages/shop/product/[slug].tsx
+++ pages/shop/product/[slug].tsx
@@ -1,5 +1,6 @@
+import Link from 'next/link';
 import { InferGetStaticPropsType } from 'next/types';
-import React from 'react';
+import React, { useRef } from 'react';
 import { clothesData } from '../../../data/data';
 import { priceComma } from '../../../utils/priceComma';
 
@@ -17,9 +18,17 @@ export const getStaticProps = async ({ params }: { params: { slug: string } }) =
     props: { product },
   };
 };
+
 const Product = ({ product }: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const sectionRef = useRef<null[] | HTMLDivElement[]>([]);
+  const moveToSection = (index: number) => {
+    window.scrollTo({
+      top: sectionRef.current[index]?.offsetTop,
+      behavior: 'smooth',
+    });
+  };
   return (
-    <div className="flex flex-col w-full h-full py-8 mx-auto max-w-7xl">
+    <div className="flex flex-col w-full h-full py-8 mx-auto gap-24 max-w-7xl">
       <div className="flex w-full h-full gap-8 justify-evenly">
         <div className="w-full after:pb-[100%] bg-mint flex justify-center items-center max-w-2xl">
           <span className="text-3xl font-semibold text-orange">IMAGE</span>
@@ -61,7 +70,79 @@ const Product = ({ product }: InferGetStaticPropsType<typeof getStaticProps>) =>
           </div>
         </div>
       </div>
-      <div className="w-full"></div>
+      <div className="w-full flex border-b-2 justify-center border-orange" ref={(element) => (sectionRef.current[0] = element)}>
+        <button
+          type="button"
+          className="w-fullp-4 px-8 flex items-center justify-center border-2 translate-y-0.5 translate-x-0.5 text-beige border-orange bg-orange"
+          onClick={() => moveToSection(0)}
+        >
+          상품정보
+        </button>
+        <button
+          type="button"
+          className="p-4 px-8 flex items-center justify-center border-2 translate-y-0.5 border-orange text-orange"
+          onClick={() => moveToSection(1)}
+        >
+          결제/교환/배송정보
+        </button>
+        <button
+          type="button"
+          className="p-4 px-8 flex items-center justify-center border-2 translate-y-0.5 -translate-x-0.5 border-orange text-orange"
+          onClick={() => moveToSection(2)}
+        >
+          상품문의 (0)
+        </button>
+      </div>
+      <div className="h-[600px] w-full flex items-center justify-center text-mint">상품정보</div>
+      <div className="w-full flex border-b-2 justify-center border-orange" ref={(element) => (sectionRef.current[1] = element)}>
+        <button
+          type="button"
+          className="p-4 px-8 flex items-center justify-center border-2 translate-y-0.5 translate-x-0.5 text-orange border-orange"
+          onClick={() => moveToSection(0)}
+        >
+          상품정보
+        </button>
+        <button
+          type="button"
+          className="p-4 px-8 flex items-center justify-center border-2 translate-y-0.5 text-beige border-orange bg-orange"
+          onClick={() => moveToSection(1)}
+        >
+          결제/교환/배송정보
+        </button>
+        <button
+          type="button"
+          className="p-4 px-8 flex items-center justify-center border-2 translate-y-0.5 -translate-x-0.5 border-orange text-orange"
+          onClick={() => moveToSection(2)}
+        >
+          상품문의 (0)
+        </button>
+      </div>
+      <div className="h-[600px] w-full flex items-center justify-center text-mint"> 결제/교환/배송정보</div>
+
+      <div className="w-full flex border-b-2 justify-center border-orange" ref={(element) => (sectionRef.current[2] = element)}>
+        <button
+          type="button"
+          className="p-4 px-8 flex items-center justify-center border-2 translate-y-0.5 translate-x-0.5 text-orange border-orange"
+          onClick={() => moveToSection(0)}
+        >
+          상품정보
+        </button>
+        <button
+          type="button"
+          className="p-4 px-8 flex items-center justify-center border-2 translate-y-0.5 border-orange text-orange"
+          onClick={() => moveToSection(1)}
+        >
+          결제/교환/배송정보
+        </button>
+        <button
+          type="button"
+          className="p-4 px-8 flex items-center justify-center border-2 translate-y-0.5 -translate-x-0.5 text-beige border-orange bg-orange"
+          onClick={() => moveToSection(2)}
+        >
+          상품문의 (0)
+        </button>
+      </div>
+      <div className="h-[600px] w-full flex items-center justify-center text-mint"> 상품문의</div>
     </div>
   );
 };
