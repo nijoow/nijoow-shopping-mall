61f0c4136e894d9c2a6be816b39d4f7e1a495986
Woojin Lee <nijoow1127@gmail.com>
2023-02-12 17:49:25 +0900

    💄 css 수정

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -12,23 +12,21 @@ export default function Layout(props: Props) {
   const router = useRouter();
 
   return (
-    <div className="">
-      <div className="flex flex-col w-full min-h-screen bg-blend-multiply bg-brown bg-texture">
-        <NavBar />{' '}
-        <AnimatePresence mode="wait" initial={false}>
-          <motion.div
-            className="flex flex-col flex-auto w-full relative"
-            key={router.asPath}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ type: 'tween', duration: 0.15, ease: 'easeOut' }}
-          >
-            {props.children}
-          </motion.div>
-        </AnimatePresence>
-        <Footer />
-      </div>
+    <div className="flex flex-col w-full min-h-screen bg-blend-multiply bg-brown bg-texture ">
+      <NavBar />{' '}
+      <AnimatePresence mode="wait" initial={false}>
+        <motion.div
+          className="flex flex-col flex-auto w-full relative"
+          key={router.asPath}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ type: 'tween', duration: 0.15, ease: 'easeOut' }}
+        >
+          {props.children}
+        </motion.div>
+      </AnimatePresence>
+      <Footer />
     </div>
   );
 }
