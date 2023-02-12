4a0af46238767fc6007994754dc213b44be8b4a9
Woojin Lee <nijoow1127@gmail.com>
2023-02-13 00:57:07 +0900

    🚧 상품페이지 상단 UI 구현

--- pages/shop/product/[slug].tsx
+++ pages/shop/product/[slug].tsx
@@ -19,15 +19,49 @@ export const getStaticProps = async ({ params }: { params: { slug: string } }) =
 };
 const Product = ({ product }: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
-    <div className="flex w-full h-full gap-4 px-4 py-8 mx-auto max-w-7xl pb-12">
-      <div className="w-full after:pb-[100%] bg-mint flex justify-center items-center ">
-        <span className="text-3xl font-semibold text-orange">IMAGE</span>
+    <div className="flex flex-col w-full h-full py-8 mx-auto max-w-7xl">
+      <div className="flex w-full h-full gap-8 justify-evenly">
+        <div className="w-full after:pb-[100%] bg-mint flex justify-center items-center max-w-2xl">
+          <span className="text-3xl font-semibold text-orange">IMAGE</span>
+        </div>
+        <div className="w-full flex flex-col justify-center max-w-lg gap-4">
+          <div className="w-full border-t-2 border-orange mb-auto"></div>
+          <span className="w-full font-medium text-beige text-4xl">
+            {product?.productName} {product?.size ? `(${product.size})` : ''}
+          </span>{' '}
+          <span className="w-full text-ocher font-medium text-3xl">{product && priceComma(product.price)}원</span>
+          <div className="w-full flex">
+            <span className="text-beige">상품 간단 설명</span>
+          </div>
+          <div className="w-full flex gap-20">
+            <span className="text-beige">적립금</span>
+            <span className="text-beige">{product && priceComma(product.price * 0.01)}원 (1%)</span>
+          </div>
+          <div className="w-full flex gap-20">
+            <span className="text-beige">배송비</span>
+            <span className="text-beige">
+              {priceComma(3000)}원 ({priceComma(50000)}원 이상 구매시 무료)
+            </span>
+          </div>
+          <div className="w-full border-t-2 border-orange mt-auto"></div>
+          <div className="w-full flex justify-between py-3">
+            <span className="font-medium text-xl text-beige">총 상품금액</span>{' '}
+            <span className="font-medium text-xl text-ocher">{product && priceComma(product.price)}원</span>
+          </div>{' '}
+          <button className="w-full p-5 bg-orange border-2 font-medium border-orange flex rounded-sm items-center justify-center text-brown" type="button">
+            바로 구매
+          </button>
+          <div className="w-full flex gap-2 ">
+            <button className="w-full p-5 border-orange border-2 font-medium flex rounded-sm items-center justify-center text-orange" type="button">
+              장바구니 담기
+            </button>
+            <button className="w-full p-5 border-orange border-2 font-medium flex rounded-sm items-center justify-center text-orange" type="button">
+              관심상품 등록
+            </button>
+          </div>
+        </div>
       </div>
-      <div className="w-full flex flex-col justify-center">
-        <span className="w-full font-medium text-beige text-3xl">{product?.productName}</span>
-        <span className="w-full font-medium text-beige">{product?.size ? `(${product.size})` : ''}</span>
-        <span className="w-full text-ocher font-medium text-2 xl">{product && priceComma(product.price)}원</span>
-      </div>
+      <div className="w-full"></div>
     </div>
   );
 };
