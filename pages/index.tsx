758be438494d0c9e03646baf0375f47102c5d9b1
Woojin Lee <nijoow1127@gmail.com>
2023-02-13 02:12:48 +0900

    ⚡️ 배너 blur 이미지 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,21 @@
-import { Auth, ThemeSupa } from '@supabase/auth-ui-react';
-import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react';
 import { NextPage } from 'next';
-import { useEffect } from 'react';
 import Image from 'next/image';
 import { clothesData } from '../data/data';
-import { priceComma } from '../utils/priceComma';
 import ProductListItem from '../components/ProductListItem';
-const Home: NextPage = () => {
+import { getPlaiceholder } from 'plaiceholder';
+
+export const getStaticProps = async () => {
+  const { base64, img } = await getPlaiceholder('/banner.jpg', { size: 10 });
+  return {
+    props: { blurDataURL: base64 },
+  };
+};
+
+const Home = ({ blurDataURL }: { blurDataURL: string }) => {
   return (
     <>
       <div className="relative w-full h-full min-h-screen">
-        <Image src={'/banner.jpg'} alt="banner" fill className="object-cover object-[0,40%] aspect-auto" />
+        <Image src={'/banner.jpg'} alt="banner" fill placeholder="blur" blurDataURL={blurDataURL} className="object-cover object-[0,40%] aspect-auto" />
         <div className="absolute inset-0 z-10 flex items-center justify-center w-full h-full text-white">
           <span className="z-20 text-8xl font-Insomnia text-beige">nijoow vintage</span>
           <span className="absolute z-10 translate-x-1 translate-y-1 text-brown/80 text-8xl font-Insomnia">nijoow vintage</span>
