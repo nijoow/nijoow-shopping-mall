2ec60da5b4d4c6e5e8aa2b42c2a5811df2b7fa89
Woojin Lee <nijoow1127@gmail.com>
2023-02-17 00:15:21 +0900

    ✨ 페이지 로딩 Spinner 추가

--- components/FullScreenLoading.tsx
+++ components/FullScreenLoading.tsx
@@ -4,7 +4,7 @@ import { AiOutlineLoading3Quarters } from 'react-icons/ai';
 const FullScreenLoading = () => {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center w-full h-full">
-      <AiOutlineLoading3Quarters className="z-50 m-auto animate-spin text-mint" size={50} />
+      <AiOutlineLoading3Quarters className="z-50 m-auto !stroke-[4px] animate-spin text-orange stroke-mint" size={50} />
     </div>
   );
 };
