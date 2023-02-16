4defe1eca8415e810bd3ad797bb1843c39eadeac
Woojin Lee <nijoow1127@gmail.com>
2023-02-16 22:56:27 +0900

    🎨 장바구니 추가 기능 추가

--- pages/shop/product/[slug].tsx
+++ pages/shop/product/[slug].tsx
@@ -1,7 +1,9 @@
 import Link from 'next/link';
 import { InferGetStaticPropsType } from 'next/types';
 import React, { useRef } from 'react';
+import { useRecoilState } from 'recoil';
 import { clothesData } from '../../../data/data';
+import { cartState } from '../../../state/cart';
 import { priceComma } from '../../../utils/priceComma';
 
 export const getStaticPaths = async () => {
@@ -20,57 +22,72 @@ export const getStaticProps = async ({ params }: { params: { slug: string } }) =
 };
 
 const Product = ({ product }: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const [cart, setCart] = useRecoilState(cartState);
   const sectionRef = useRef<null[] | HTMLDivElement[]>([]);
+
   const moveToSection = (index: number) => {
     window.scrollTo({
       top: sectionRef.current[index]?.offsetTop,
       behavior: 'smooth',
     });
   };
+
+  const addCart = (productId: number) => {
+    if (cart.find((item) => item.id === productId)) {
+      alert('이미 장바구니에 존재하는 상품입니다');
+      return;
+    }
+    product && setCart([...cart, product]);
+  };
+
   return (
-    <div className="flex flex-col w-full h-full py-8 mx-auto gap-24 max-w-7xl">
+    <div className="flex flex-col w-full h-full gap-24 py-8 mx-auto max-w-7xl">
       <div className="flex w-full h-full gap-8 justify-evenly">
         <div className="w-full after:pb-[100%] bg-mint flex justify-center items-center max-w-2xl">
           <span className="text-3xl font-semibold text-orange">IMAGE</span>
         </div>
-        <div className="w-full flex flex-col justify-center max-w-lg gap-4">
-          <div className="w-full border-t-2 border-orange mb-auto"></div>
-          <span className="w-full font-medium text-beige text-4xl">
+        <div className="flex flex-col justify-center w-full max-w-lg gap-4">
+          <div className="w-full mb-auto border-t-2 border-orange"></div>
+          <span className="w-full text-4xl font-medium text-beige">
             {product?.productName} {product?.size ? `(${product.size})` : ''}
           </span>{' '}
-          <span className="w-full text-ocher font-medium text-3xl">{product && priceComma(product.price)}원</span>
-          <div className="w-full flex">
+          <span className="w-full text-3xl font-medium text-ocher">{product && priceComma(product.price)}원</span>
+          <div className="flex w-full">
             <span className="text-beige">상품 간단 설명</span>
           </div>
-          <div className="w-full flex gap-20">
+          <div className="flex w-full gap-20">
             <span className="text-beige">적립금</span>
             <span className="text-beige">{product && priceComma(product.price * 0.01)}원 (1%)</span>
           </div>
-          <div className="w-full flex gap-20">
+          <div className="flex w-full gap-20">
             <span className="text-beige">배송비</span>
             <span className="text-beige">
               {priceComma(3000)}원 ({priceComma(50000)}원 이상 구매시 무료)
             </span>
           </div>
-          <div className="w-full border-t-2 border-orange mt-auto"></div>
-          <div className="w-full flex justify-between py-3">
-            <span className="font-medium text-xl text-beige">총 상품금액</span>{' '}
-            <span className="font-medium text-xl text-ocher">{product && priceComma(product.price)}원</span>
+          <div className="w-full mt-auto border-t-2 border-orange"></div>
+          <div className="flex justify-between w-full py-3">
+            <span className="text-xl font-medium text-beige">총 상품금액</span>{' '}
+            <span className="text-xl font-medium text-ocher">{product && priceComma(product.price)}원</span>
           </div>{' '}
-          <button className="w-full p-5 bg-orange border-2 font-medium border-orange flex rounded-sm items-center justify-center text-brown" type="button">
+          <button className="flex items-center justify-center w-full p-5 font-medium border-2 rounded-sm bg-orange border-orange text-brown" type="button">
             바로 구매
           </button>
-          <div className="w-full flex gap-2 ">
-            <button className="w-full p-5 border-orange border-2 font-medium flex rounded-sm items-center justify-center text-orange" type="button">
+          <div className="flex w-full gap-2 ">
+            <button
+              className="flex items-center justify-center w-full p-5 font-medium border-2 rounded-sm border-orange text-orange"
+              type="button"
+              onClick={() => addCart(product?.id)}
+            >
               장바구니 담기
             </button>
-            <button className="w-full p-5 border-orange border-2 font-medium flex rounded-sm items-center justify-center text-orange" type="button">
+            <button className="flex items-center justify-center w-full p-5 font-medium border-2 rounded-sm border-orange text-orange" type="button">
               관심상품 등록
             </button>
           </div>
         </div>
       </div>
-      <div className="w-full flex border-b-2 justify-center border-orange" ref={(element) => (sectionRef.current[0] = element)}>
+      <div className="flex justify-center w-full border-b-2 border-orange" ref={(element) => (sectionRef.current[0] = element)}>
         <button
           type="button"
           className="w-fullp-4 px-8 flex items-center justify-center border-2 translate-y-0.5 translate-x-0.5 text-beige border-orange bg-orange"
@@ -94,7 +111,7 @@ const Product = ({ product }: InferGetStaticPropsType<typeof getStaticProps>) =>
         </button>
       </div>
       <div className="h-[600px] w-full flex items-center justify-center text-mint">상품정보</div>
-      <div className="w-full flex border-b-2 justify-center border-orange" ref={(element) => (sectionRef.current[1] = element)}>
+      <div className="flex justify-center w-full border-b-2 border-orange" ref={(element) => (sectionRef.current[1] = element)}>
         <button
           type="button"
           className="p-4 px-8 flex items-center justify-center border-2 translate-y-0.5 translate-x-0.5 text-orange border-orange"
@@ -119,7 +136,7 @@ const Product = ({ product }: InferGetStaticPropsType<typeof getStaticProps>) =>
       </div>
       <div className="h-[600px] w-full flex items-center justify-center text-mint"> 결제/교환/배송정보</div>
 
-      <div className="w-full flex border-b-2 justify-center border-orange" ref={(element) => (sectionRef.current[2] = element)}>
+      <div className="flex justify-center w-full border-b-2 border-orange" ref={(element) => (sectionRef.current[2] = element)}>
         <button
           type="button"
           className="p-4 px-8 flex items-center justify-center border-2 translate-y-0.5 translate-x-0.5 text-orange border-orange"
