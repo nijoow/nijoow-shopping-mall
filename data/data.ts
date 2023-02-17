06cfc77dab8eef8d13ec7161b24dcf886a2b397e
Woojin Lee <nijoow1127@gmail.com>
2023-02-18 00:03:07 +0900

    🌱 dummy data 수정

--- data/data.ts
+++ data/data.ts
@@ -1,10 +1,10 @@
 export const clothesData = [
-  { productName: '맨투맨 01', price: 39000, size: 'L', category: 'Top', id: 1 },
-  { productName: '맨투맨 02', price: 49000, size: 'XL', category: 'Top', id: 2 },
-  { productName: '후드 집업 01', price: 85000, size: 'M', category: 'Outer', id: 3 },
-  { productName: '후드티 01', price: 41000, size: 'L', category: 'Top', id: 4 },
-  { productName: '청바지 01', price: 70000, size: 'S', category: 'Bottom', id: 5 },
-  { productName: '청바지 02', price: 60000, size: 'L', category: 'Bottom', id: 6 },
-  { productName: '반지 01', price: 70000, size: '20', category: 'Acc', id: 7 },
-  { productName: '가방 01', price: 60000, size: null, category: 'Acc', id: 8 },
+  { productName: '맨투맨 01', price: 39000, size: 'L', category: 'Top', id: 20230001 },
+  { productName: '맨투맨 02', price: 49000, size: 'XL', category: 'Top', id: 20230002 },
+  { productName: '후드 집업 01', price: 85000, size: 'M', category: 'Outer', id: 20230003 },
+  { productName: '후드티 01', price: 41000, size: 'L', category: 'Top', id: 20230004 },
+  { productName: '청바지 01', price: 70000, size: 'S', category: 'Bottom', id: 20230005 },
+  { productName: '청바지 02', price: 60000, size: 'L', category: 'Bottom', id: 20230006 },
+  { productName: '반지 01', price: 70000, size: '20', category: 'Acc', id: 20230007 },
+  { productName: '가방 01', price: 60000, size: null, category: 'Acc', id: 20230008 },
 ];
